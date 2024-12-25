2de97c0fa2efcff4cd925757fe51b6c7d255467a
Nika <srkveronica@gmail.com>
2024-12-25 16:34:04 +0300

    Add filter by category

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Expense Tracker API
+
+This is a Django REST Framework (DRF) application for managing expenses and categories. It provides endpoints for creating, retrieving, updating, and deleting expenses, as well as managing categories.
+
--- expense_tracker/catalog/filters.py
+++ expense_tracker/catalog/filters.py
@@ -1,5 +1,5 @@
 import django_filters
-from .models import Expense
+from .models import Expense, Category
 
 
 class ExpenseFilter(django_filters.FilterSet):
@@ -7,6 +7,9 @@ class ExpenseFilter(django_filters.FilterSet):
                                                lookup_expr='gte')
     end_date = django_filters.DateTimeFilter(field_name='createdAt',
                                              lookup_expr='lte')
+    category = django_filters.CharFilter(field_name='category_id__category',
+                                          lookup_expr='iexact')
+    
     class Meta:
         model = Expense
-        fields = ['start_date', 'end_date']
+        fields = ['start_date', 'end_date', 'category']
